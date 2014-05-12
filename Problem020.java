4c2e5732e25e92c0ec29d3c43e8aebdd742219d5
arjunsubway <arjunsubway@gmail.com>
2014-05-12 12:02:15 -0700

    Update Problem020.java

--- Problem020.java
+++ Problem020.java
@@ -3,14 +3,18 @@ public class Problem20 {
 	
 	public static void main(String[] args) {
 		
+		String temp = factorial(100).toString();
+		int sum = 0;
+		for(int i = 0; i < temp.length(); i++) {
+			sum += temp.charAt(i) - '0';
+		}
 		
 		
-		
-	
+	      System.out.println(sum);
 	}
 	
-	public static int factorial(int n) {
-		int base = 1;
+	public static long factorial(long n) {
+		long base = 1;
 		
 		for(int i = 1; i <= n; i++) {
 			base *= i;
